d3d26599ff1994613a994d1fc1c17744d2435b30
cyh1219 <cyh1219@naver.com>
2021-04-18 23:46:34 +0900

    03-01.jpa init

---
+++ src/main/java/com/management/management/repository/PersonRepository.java
@@ -0,0 +1,9 @@
+package com.management.management.repository;
+
+import com.management.management.domain.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+
+}
---
+++ src/test/java/com/management/management/repository/PersonRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.management.management.repository;
+
+import com.management.management.domain.Person;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PersonRepositoryTest {
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Test
+    void crud() {
+        Person person = new Person();
+        person.setName("martin");
+        person.setAge(10);
+
+        personRepository.save(person);
+
+//        System.out.println(personRepository.findAll());
+
+        List<Person> people = personRepository.findAll();
+        //전체 데이터를 다 가지고 오겠다는 뜻.
+
+        assertThat(people.size()).isEqualTo(1);
+        assertThat(people.get(0).getName()).isEqualTo("martin");
+        assertThat(people.get(0).getAge()).isEqualTo(10);
+    }
+}
